359069681d3798eab508d15bfc873673c9359c1b
wenboshen-echoud <echoud16@gmail.com>
2018-03-17 23:16:05 -0700

    minor change

--- voice_api.py
+++ voice_api.py
@@ -1,6 +1,7 @@
 import hashlib
 import hmac
 import sys
+import requests
 
 #curl -X POST -H "Content-Type: application/json" -d '{
   #"message": {
@@ -15,6 +16,23 @@ import sys
   #},
   #"signature": "830e4c7a0fc3a46ef51db769c11a796cd82208391a54d34443f9a31ee65aa522"
 #}' "http://api.0mzl.com/v2/message"
+
+def invoke_ap():
+    req_json= {
+    "channel_uuid": "XXX",
+    "service_uuid": "a5273c4b-1d39-4594-ae59-0748b317da2a",
+    "user_uuid": "USER_UUID",
+    "type": "text",
+    "data": {
+      "text": "北京今天天气怎么样"
+    },
+    "time": 1521261146704
+    }
+    "signature": "830e4c7a0fc3a46ef51db769c11a796cd82208391a54d34443f9a31ee65aa522"
+
+    req = requests.post('http://api.0mzl.com/v2/message', json={"key": "value"})
+
+
 
 def get_signature(channel_uuid, service_uuid, user_uuid, time, client_secret):
     content = '{}{}{}{}'.format(channel_uuid, service_uuid or '', user_uuid, time)
